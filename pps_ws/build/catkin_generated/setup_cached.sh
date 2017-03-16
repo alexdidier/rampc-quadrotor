1a18d6c85d2298c9145b1192d9cc79d393db4cb1
Philipp Friedli <phfriedl@student.ethz.ch>
2017-03-16 10:32:52 +0100

    added Vicon libraries

---
+++ pps_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/crazyflie/D-FaLL-System/pps_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/crazyflie/D-FaLL-System/pps_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/crazyflie/D-FaLL-System/pps_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/crazyflie/D-FaLL-System/pps_ws/src:$ROS_PACKAGE_PATH"
---
+++ pps_ws/build/d_fall_pps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/crazyflie/D-FaLL-System/pps_ws/src/d_fall_pps
+# Build directory: /home/crazyflie/D-FaLL-System/pps_ws/build/d_fall_pps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
