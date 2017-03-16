05f27bf44df8e6f75ab54468e99e366d8cb9fa60
Philipp Friedli <phfriedl@student.ethz.ch>
2017-03-16 10:42:25 +0100

    Revert "added Vicon libraries"
    This reverts commit 1a18d6c85d2298c9145b1192d9cc79d393db4cb1.

--- pps_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/crazyflie/D-FaLL-System/pps_ws/src
-# Build directory: /home/crazyflie/D-FaLL-System/pps_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(d_fall_pps)
