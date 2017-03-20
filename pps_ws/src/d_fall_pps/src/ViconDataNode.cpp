4f836caae59de5a737c6d92b2b4ce38d20a967ae
bucyril <bucyril@student.ethz.ch>
2017-03-20 15:52:57 +0100

    ViconDataNode now working, added license, started PPSClient

--- pps_ws/src/d_fall_pps/src/ViconDataNode.cpp
+++ pps_ws/src/d_fall_pps/src/ViconDataNode.cpp
@@ -12,136 +12,108 @@
 #include <string.h>
 #include "DataStreamClient.h"
 #include "ros/ros.h"
+#include "d_fall_pps/ViconData.h"
 
 using namespace ViconDataStreamSDK::CPP;
 
-void connect(Client* client) {
-    std::string hostName = "10.42.00.15:801";
-	ROS_INFO_STREAM("Connecting to " << hostName << " ...");
-    while( !client->IsConnected().Connected )
-    {
-        bool ok =( client->Connect( hostName ).Result == Result::Success );
-
-        if(!ok)
-        {
-            ROS_ERROR("Warning - connection failed...");
-            ros::Duration(1.0).sleep();
-        }
-        else
-        {
-            ROS_INFO("Connected successfully");
-        }
-    }
-}
-
-void setupDataStream(Client* client) {
-	// Enable some different data types
-        ROS_INFO_STREAM("ClientPull stream mode enabled3");
-        client->EnableSegmentData();
-        client->EnableMarkerData();
-        client->EnableUnlabeledMarkerData();
-        client->EnableDeviceData();
-        ROS_INFO_STREAM("ClientPull stream mode enabled4");
-
-        // Set the streaming mode
-        client->SetStreamMode( ViconDataStreamSDK::CPP::StreamMode::ClientPull );
-        ROS_INFO_STREAM("ClientPull stream mode enabled");
-
-        // Set the global up axis
-        client->SetAxisMapping( Direction::Forward,
-                                 Direction::Left,
-                                 Direction::Up ); // Z-up
-        // client->SetGlobalUpAxis( Direction::Forward,
-        //                           Direction::Up,
-        //                           Direction::Right ); // Y-up
-}
-
-int main( int argc, char* argv[] )
-{
-    ros::init(argc, argv, "ViconDataStreamSDK");
-    ROS_INFO_STREAM("Okili dokili");
+int main(int argc, char* argv[]) {
+    ros::init(argc, argv, "ViconDataNode");
 
     ros::NodeHandle nodeHandle("~");
     ros::Time::init();
 
+    ros::Publisher viconDataPublisher =
+            nodeHandle.advertise<d_fall_pps::ViconData>("ViconData", 1);
+
     Client client;
-    connect(&client);
-    setupDataStream(&client);
 
-    while(ros::ok()) {
-    	            // Get a frame
-            while( client.GetFrame().Result != Result::Success )
-            {
-                // Sleep a little so that we don't lumber the CPU with a busy poll
-                ros::Duration(0.001).sleep();
-            }
-            
-            // Count the number of subjects
-            unsigned int subjectCount = client.GetSubjectCount().SubjectCount;
+    //connect client to Vicon computer
+    std::string hostName = "10.42.00.15:801";
+    ROS_INFO_STREAM("Connecting to " << hostName << " ...");
+    while (!client.IsConnected().Connected) {
+        bool ok = (client.Connect(hostName).Result == Result::Success);
 
-            // //Process the data and publish on topic
-             for(int index = 0 ; index < subjectCount ; index++ )
-             {
-             	std::string subjName = client.GetSubjectName( index ).SubjectName;
-             	std::string segName = client.GetSegmentName( subjName, index ).SegmentName;
-
-                 ROS_INFO_STREAM(subjName);
-                 ROS_INFO_STREAM(segName);
-                 ROS_INFO_STREAM("------");
-             }
-
+        if (!ok) {
+            ROS_ERROR("Error - connection failed...");
+            ros::Duration(1.0).sleep();
+        } else {
+            ROS_INFO("Connected successfully");
+        }
     }
-/*
-        while( ros::ok())
-        {
-            // Get a frame
-            while( client.GetFrame().Result != Result::Success )
-            {
-                // Sleep a little so that we don't lumber the CPU with a busy poll
-                ros::Duration(0.001).sleep();
-            }
 
-            //Get frame rate
-            //            Output_GetFrameRate OutputFrameRate = client.GetFrameRate ();
-            //            if(OutputFrameRate.Result==Result::Success)
-            //                std::cout<<"Frame Rate= "<<OutputFrameRate.FrameRateHz<<std::endl;
-            //            else
-            //                std::cout<<"Frame Rate not fetched ERROR"<<std::endl;
+    //set data stream parameters
+    client.SetStreamMode(ViconDataStreamSDK::CPP::StreamMode::ClientPull);
 
-            // //Get latency
-            // OutputLatencyTotal = client.GetLatencyTotal();
-            // if(OutputLatencyTotal.Result==Result::Success)
-            //     totalViconLatency=OutputLatencyTotal.Total;
-            // else
-            //     totalViconLatency=0;
-            // //std::cout<<"Total latency: "<<totalViconLatency;
-            // dataAcquiringTime=loopStartTime-ros::Duration(totalViconLatency);
+    client.EnableSegmentData();
+    client.EnableMarkerData();
+    client.EnableUnlabeledMarkerData();
+    client.EnableDeviceData();
 
+    // Set the global up axis
+    client.SetAxisMapping(Direction::Forward,
+            Direction::Left,
+            Direction::Up);
 
-            // Count the number of subjects
-            int subjectCount = client.GetSubjectCount().SubjectCount;
-            ROS_INFO_STREAM();
-
-            // //Process the data and publish on topic
-            // for( SubjectIndex = 0 ; SubjectIndex < SubjectCount ; ++SubjectIndex )
-            // {
-
-            //     // Get the subject's name
-            //     subjectName = client.GetSubjectName( subjectIndex ).SubjectName;
-            //     segmentName = client.GetSegmentName( subjectName, subjectIndex ).SegmentName;
-            //     ROS_INFO_STREAM();
-            // }
+    while (ros::ok()) {
+        // Get a frame
+        while (client.GetFrame().Result != Result::Success) {
+            // Sleep a little so that we don't lumber the CPU with a busy poll
+            ros::Duration(0.001).sleep();
         }
 
-        client.DisableSegmentData();
-        client.DisableMarkerData();
-        client.DisableUnlabeledMarkerData();
-        client.DisableDeviceData();
+        unsigned int subjectCount = client.GetSubjectCount().SubjectCount;
 
+        // //Process the data and publish on topic
+        for (int index = 0; index < subjectCount; index++) {
+            std::string subjectName = client.GetSubjectName(index).SubjectName;
+            std::string segmentName = client.GetSegmentName(subjectName, index).SegmentName;
 
-        // Disconnect and dispose
-        ROS_WARN_STREAM(" Disconnecting..." << std::endl);
-        client.Disconnect();
-        ROS_WARN_STREAM(" Disconnected" << std::endl);
-*/
+            Output_GetSegmentGlobalTranslation outputTranslation =
+                    client.GetSegmentGlobalTranslation(subjectName, segmentName);
+
+            Output_GetSegmentGlobalRotationQuaternion outputRotation =
+                    client.GetSegmentGlobalRotationQuaternion(subjectName, segmentName);
+
+            //calculate position and rotation of Crazyflie
+            double quat_x = outputRotation.Rotation[0];
+            double quat_y = outputRotation.Rotation[1];
+            double quat_z = outputRotation.Rotation[2];
+            double quat_w = outputRotation.Rotation[3];
+
+            //TODO check whether this transformation is correct
+            double roll = atan2(2 * (quat_w * quat_x + quat_y * quat_z), 1 - 2 * (quat_x * quat_x + quat_y * quat_y));
+            double pitch = asin(2 * (quat_w * quat_y - quat_z * quat_x));
+            double yaw = atan2(2 * (quat_w * quat_z + quat_x * quat_y), 1 - 2 * (quat_y * quat_y + quat_z * quat_z));
+
+            //calculate time until frame data was received
+            Output_GetLatencyTotal outputLatencyTotal = client.GetLatencyTotal();
+            double totalViconLatency;
+            if (outputLatencyTotal.Result == Result::Success) {
+                totalViconLatency = outputLatencyTotal.Total;
+            } else {
+                totalViconLatency = 0;
+            }
+
+            //build and publish message
+            d_fall_pps::ViconData viconData;
+            viconData.crazyflieName = subjectName;
+            viconData.x = outputTranslation.Translation[0];
+            viconData.y = outputTranslation.Translation[1];
+            viconData.z = outputTranslation.Translation[2];
+            viconData.roll = roll;
+            viconData.pitch = pitch;
+            viconData.yaw = yaw;
+            viconData.acquiringTime = totalViconLatency;
+
+            viconDataPublisher.publish(viconData);
+        }
+
+    }
+
+    client.DisableSegmentData();
+    client.DisableMarkerData();
+    client.DisableUnlabeledMarkerData();
+    client.DisableDeviceData();
+
+    client.Disconnect();
 }
