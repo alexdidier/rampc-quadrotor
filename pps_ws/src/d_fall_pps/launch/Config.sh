805e007f9fd6ec2fe6cf3abc78ab07ade4106b82
bucyril <bucyril@ethz.ch>
2017-05-29 14:15:30 +0200

    created install script, removed shitstack.txt, corrected small compilation error for CentralManagerService.cpp

--- pps_ws/src/d_fall_pps/launch/Config.sh
+++ pps_ws/src/d_fall_pps/launch/Config.sh
@@ -1,5 +1,5 @@
 export ROS_MASTER_URI=http://teacher:11311
 export ROS_IP=10.42.0.33
-export ROS_HOSTNAME=student$(cat ../param/StudentID)
-export ROS_NAMESPACE=$(cat ../param/StudentID)
+export ROS_HOSTNAME=student$(cat /etc/StudentID)
+export ROS_NAMESPACE=$(cat /etc/StudentID)
 
