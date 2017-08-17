ee751c82debd17fbdf2d17b43b4a5b681f7191ac
roangel <roangel@student.ethz.ch>
2017-08-17 11:46:58 +0200

    starts client without starting drone. Then scripts

--- pps_ws/src/d_fall_pps/src/PPSClient.cpp
+++ pps_ws/src/d_fall_pps/src/PPSClient.cpp
@@ -277,7 +277,7 @@ int main(int argc, char* argv[]){
     ros::Subscriber commandSubscriber = nodeHandle.subscribe("Command", 1, commandCallback);
 
 	//start with safe controller
-	crazyflieEnabled = true;
+	crazyflieEnabled = false;
 	usingSafeController = true;
 	loadSafeController();
 
