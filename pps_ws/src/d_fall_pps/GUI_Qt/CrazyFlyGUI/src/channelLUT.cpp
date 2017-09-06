789e11b41193ed06a7d20f4de699820a41b58df4
roangel <roangel@student.ethz.ch>
2017-09-06 10:34:34 +0200

    changed LUT for new CFs

--- pps_ws/src/d_fall_pps/GUI_Qt/CrazyFlyGUI/src/channelLUT.cpp
+++ pps_ws/src/d_fall_pps/GUI_Qt/CrazyFlyGUI/src/channelLUT.cpp
@@ -10,5 +10,12 @@ std::map<std::string, std::string> channel_LUT
     {"cfTwo", "0/69/2M"},
     {"cfThree", "0/72/2M"},
     {"cfFour", "0/99/2M"},
-    {"PPS_CF1", "0/0/2M"},
+    {"PPS_CF01", "0/0/2M"},
+    {"PPS_CF02", "0/8/2M"},
+    {"PPS_CF03", "0/16/2M"},
+    {"PPS_CF04", "0/24/2M"},
+    {"PPS_CF05", "0/32/2M"},
+    {"PPS_CF06", "0/40/2M"},
+    {"PPS_CF07", "0/48/2M"},
+    {"PPS_CF08", "0/56/2M"},
 };
