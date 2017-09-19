281c07cc9712ec8c2584c4e533d5b69ad8b0a4af
Angel <roangel@student.ethz.ch>
2017-09-19 20:30:10 +0200

    address stuff

--- pps_ws/src/d_fall_pps/GUI_Qt/CrazyFlyGUI/src/channelLUT.cpp
+++ pps_ws/src/d_fall_pps/GUI_Qt/CrazyFlyGUI/src/channelLUT.cpp
@@ -6,7 +6,7 @@ std::map<std::string, std::string> channel_LUT
     // {"CF1", "A12D2"},
     // {"CF2", "E341E"},
     // {"CF3", "4E21A"},
-    {"cfOne", "0/76/2M"},
+    {"cfOne", "0/76/2M/E7E7E7E701"},
     {"cfTwo", "0/69/2M"},
     {"cfThree", "0/72/2M"},
     {"cfFour", "0/99/2M"},
