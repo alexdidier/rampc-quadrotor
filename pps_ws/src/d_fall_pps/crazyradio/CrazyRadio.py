a82674038599deda70c33c9e4df88364e7de5bdb
beuchatp <beuchatp@control.ee.ethz.ch>
2017-10-31 23:14:21 +0100

    Adapted license header to match with the ETH Zurich requirements

--- pps_ws/src/d_fall_pps/crazyradio/CrazyRadio.py
+++ pps_ws/src/d_fall_pps/crazyradio/CrazyRadio.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Alternate controller that is expected to work.
-# Copyright (C) 2017  Angel Romero, Cyrill Burgener, Marco Mueller, Philipp Friedli
+#    Copyright (C) 2017, ETH Zurich, D-ITET, Angel Romero, Cyrill Burgener, Marco Mueller, Philipp Friedli
+#
+#    This file is part of D-FaLL-System.
+#    
+#    D-FaLL-System is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#    
+#    D-FaLL-System is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    
+#    You should have received a copy of the GNU General Public License
+#    along with D-FaLL-System.  If not, see <http://www.gnu.org/licenses/>.
+#    
+#
+#    ----------------------------------------------------------------------------------
+#    DDDD        FFFFF        L     L           SSSS  Y   Y   SSSS  TTTTT  EEEEE  M   M
+#    D   D       F      aaa   L     L          S       Y Y   S        T    E      MM MM
+#    D   D  ---  FFFF  a   a  L     L     ---   SSS     Y     SSS     T    EEE    M M M
+#    D   D       F     a  aa  L     L              S    Y        S    T    E      M   M
+#    DDDD        F      aa a  LLLL  LLLL       SSSS     Y    SSSS     T    EEEEE  M   M
+#
+#
+#    DESCRIPTION:
+#    The service that manages the context of the student groups.
+#
+#    ----------------------------------------------------------------------------------
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import roslib; roslib.load_manifest('d_fall_pps')
 import rospy
