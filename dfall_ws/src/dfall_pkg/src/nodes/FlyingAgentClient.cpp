fbade28c49d249f71303a79569cadbdbabaea3a6
beuchatp <beuchatp@control.ee.ethz.ch>
2019-02-14 10:50:50 +0100

    Adjusted Flying Agent Client header to have correct copyright details

--- dfall_ws/src/dfall_pkg/src/nodes/FlyingAgentClient.cpp
+++ dfall_ws/src/dfall_pkg/src/nodes/FlyingAgentClient.cpp
@@ -1,4 +1,5 @@
-//    Copyright (C) 2019, ETH Zurich, D-ITET, Paul Beuchat, Angel Romero, Cyrill Burgener, Marco Mueller, Philipp Friedli
+//    Copyright (C) 2019, ETH Zurich, D-ITET, Paul Beuchat
+//    Copyright (C) 2017, ETH Zurich, D-ITET, Paul Beuchat, Angel Romero, Cyrill Burgener, Marco Mueller, Philipp Friedli
 //
 //    This file is part of D-FaLL-System.
 //    
